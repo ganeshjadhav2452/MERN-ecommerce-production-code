6d7b04d25d1b1bd6e14c02bbbc6546d1876342b5
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-10-22 20:26:40 +0530

    another changes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import cors from 'cors'
 import categoryRoutes from './routes/categoryRoutes.js'
 import ProductRoutes from './routes/productRoutes.js'
 import path from 'path'
+import { fileURLToPath } from 'url';
 // configuring env file
 dotenv.config()
 
@@ -20,6 +21,10 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+
+// es6 fixed
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 //rest api
 app.use('/api/v1/auth', authRoutes)
